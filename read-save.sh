aefbcf8d8c0d7588b302ba27b5595cd5f2097203
mkufel <m.kufel@hotmail.com>
2018-11-28 07:53:22 +0100

    Renamed google results and change the measure cpu file name in read-save

--- read-save.sh
+++ read-save.sh
@@ -4,7 +4,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 echo $(printf "%s\n" $(date +%s)",";./read.sh) >> measure-disk-sequential.csv
 echo $(printf "%s\n" $(date +%s)",";./read-random.sh) >> measure-disk-random.csv
 echo $(printf "%s\n" $(date +%s)",";./memsweep-edit.sh) >> memsweep-mem.csv
-echo $(printf "%s\n" $(date +%s)",";./linpack-edit.sh) >> memsweep-cpu.csv
+echo $(printf "%s\n" $(date +%s)",";./linpack-edit.sh) >> measure-cpu.csv
 
 
 
