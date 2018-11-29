d1d7b26263e3a3d7f9b38833db0fb031cecc0310
mkufel <m.kufel@hotmail.com>
2018-11-29 19:21:41 +0100

    +3

--- submission-gp27/read-random-resource.sh
+++ submission-gp27/read-random-resource.sh
@@ -3,6 +3,6 @@
 # grep the line containing aggregated read speed result
 # get the 3rd item of the output line (aggrb)
 # Leave only digits in the output to get a read speed in KB/s
-var=$(fio --name=randread --ioengine=libaio --iodepth=2 --rw=randread --bs=4k --direct=0 --size=16M --numjobs=1 --runtime=10 --group_reporting | grep aggrb | awk '{printf $3}' | sed 's/[^0-9]//g')
+var=$(fio --name=randread --ioengine=libaio --iodepth=2 --rw=randread --bs=4k --direct=0 --size=16M --numjobs=1 --runtime=15 --group_reporting | grep aggrb | awk '{printf $3}' | sed 's/[^0-9]//g')
 # multiply by 1024 to get a number of bytes/s
 echo $(($var * 1024))
